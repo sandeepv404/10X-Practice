8cc9a23ba292009a5f2c41bb3312c0f1341fdeab
Sandeep Kumar Vollala <68808142+sandeepv404@users.noreply.github.com>
2022-10-24 11:16:24 +0530

    Update luckynum.py

--- luckynum.py
+++ luckynum.py
@@ -1,23 +1,16 @@
 def findLuckyNumber(nums):
-
-
-n = int(input())
-array = []
-for i in range(0, n):
-    list = array.append(int(input()))
-for j in range(0, len(array)):
     count = 0
-    for k in range(0, len(array)):
-        if array[j] == list[i]:
-            count = count + 1
-    if count == array[j]:
-        print(array[j])
+    for j in range(0, len(nums)):
+        for k in range(0, len(nums)):
+            if nums[j] == nums[k]:
+                count = count + 1
+            if count == nums[j]:
+                return nums[j]
 
-    # DO NOT change anything below this line
 if __name__ == "__main__":
     num_elems = int(input())
     input_arr = []
     for index in range(num_elems):
         input_arr.append(int(input()))
-
-    print(findLuckyNumber(input_arr))
+    
+    print(findLuckyNumber(input_arr))  
